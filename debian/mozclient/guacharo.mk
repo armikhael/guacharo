756b90e70085eceb47b729abf14d293dc6eb339c
Luis Alejandro Martínez Faneyth <martinez.faneyth@gmail.com>
2011-09-28 23:57:06 -0430

    Ejecutando script de conversión de Icedove a Guácharo

--- debian/mozclient/guacharo.mk
+++ debian/mozclient/guacharo.mk
@@ -1,7 +1,7 @@
 # -*- mode: makefile; coding: utf-8 -*-
 
 # Copyright (c) 2009 Guido Guenther <agx@sigxcpu.org>
-# Description: Project Icedove 3.0
+# Description: Project Guacharo 3.0
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
@@ -17,22 +17,22 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-BRANDING_DIR = mail/branding/icedove/
+BRANDING_DIR = mail/branding/guacharo/
 
-MOZCLIENT_PROJECTNAME := icedove
+MOZCLIENT_PROJECTNAME := guacharo
 include debian/mozclient/thunderbird.mk
 
-makebuilddir/$(DEB_MOZ_APPLICATION):: debian/stamp-icedove-branding
-debian/stamp-icedove-branding:
-	cp -af debian/icedove-branding $(BRANDING_DIR)
-	cp debian/app-icons/icedove16.png $(BRANDING_DIR)/mailicon16.png
-	cp debian/app-icons/icedove22.png $(BRANDING_DIR)/mailicon22.png
-	cp debian/app-icons/icedove24.png $(BRANDING_DIR)/mailicon24.png
-	cp debian/app-icons/icedove32.png $(BRANDING_DIR)/mailicon32.png
-	cp debian/app-icons/icedove48.png $(BRANDING_DIR)/mailicon48.png
-	cp debian/app-icons/icedove256.png $(BRANDING_DIR)/mailicon256.png
-	cp debian/app-icons/icedove48.png $(BRANDING_DIR)/content/icon48.png
-	cp debian/app-icons/icedove64.png $(BRANDING_DIR)/content/icon64.png
+makebuilddir/$(DEB_MOZ_APPLICATION):: debian/stamp-guacharo-branding
+debian/stamp-guacharo-branding:
+	cp -af debian/guacharo-branding $(BRANDING_DIR)
+	cp debian/app-icons/guacharo16.png $(BRANDING_DIR)/mailicon16.png
+	cp debian/app-icons/guacharo22.png $(BRANDING_DIR)/mailicon22.png
+	cp debian/app-icons/guacharo24.png $(BRANDING_DIR)/mailicon24.png
+	cp debian/app-icons/guacharo32.png $(BRANDING_DIR)/mailicon32.png
+	cp debian/app-icons/guacharo48.png $(BRANDING_DIR)/mailicon48.png
+	cp debian/app-icons/guacharo256.png $(BRANDING_DIR)/mailicon256.png
+	cp debian/app-icons/guacharo48.png $(BRANDING_DIR)/content/icon48.png
+	cp debian/app-icons/guacharo64.png $(BRANDING_DIR)/content/icon64.png
 	cp debian/preview.png mail/themes/gnomestripe/mail/preview.png
 	touch $@
 
